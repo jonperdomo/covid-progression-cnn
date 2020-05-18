fb91c515e69995022b27cea103272c1b39708794
jonperdomo <jonperdomodb@gmail.com>
2020-05-18 00:51:25 -0400

    ENH: Add epochs as CLI argument

--- covid_ct_vgg.py
+++ covid_ct_vgg.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import numpy as np
 import tensorflow as tf
@@ -15,25 +16,6 @@ from keras.layers.convolutional import *
 from matplotlib import pyplot as plt
 from sklearn.metrics import confusion_matrix
 import itertools
-
-# Don't pre-allocate GPU memory (Tensorflow 2)
-gpus = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(gpus[0], True)
-
-output_path = 'Models/'
-train_path = 'Q2/Train/'
-valid_path = 'Q2/Valid/'
-test_path = 'Q2/Test'
-labels = ['G7', 'LE7']
-
-# 112 training images
-train_batches = ImageDataGenerator().flow_from_directory(train_path, target_size=(224, 224), classes=labels, batch_size=8)
-
-# 16 validation images
-valid_batches = ImageDataGenerator().flow_from_directory(valid_path, target_size=(224, 224), classes=labels, batch_size=8)
-
-# 14 test images
-test_batches = ImageDataGenerator().flow_from_directory(test_path, target_size=(224, 224), classes=labels, batch_size=7)
 
 
 # plots images with labels within jupyter notebook
@@ -52,35 +34,6 @@ def plots(ims, figsize=(12,6), rows=1, interp=False, titles=None):
             sp.set_title(title, fontsize=16)
         plt.imshow(ims[i], interpolation=None if interp else 'none')
     plt.show()
-
-
-# imgs, labels = next(train_batches)
-# plots(imgs, titles=labels)
-
-# Build and train CNN
-
-# # Create model
-# model = Sequential([
-#     Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
-#     Flatten(),
-#     Dense(2, activation='softmax')
-# ])
-#
-# # Compile
-# model.compile(Adam(lr=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
-#
-# # Train
-# model.fit_generator(train_batches, steps_per_epoch=7, validation_data=valid_batches, validation_steps=2, epochs=5, verbose=2)
-#
-# # Predict
-# test_imgs, test_labels = next(test_batches)
-# plots(test_imgs, titles=test_labels)
-#
-# test_labels = test_labels[:, 0]
-# predictions = model.predict_generator(test_batches, steps=2, verbose=0)
-#
-# # Confusion matrix
-# cm = confusion_matrix(test_labels, predictions[:, 0])
 
 
 # Plot
@@ -119,48 +72,76 @@ def plot_confusion_matrix(cm, classes,
     plt.show()
 
 
-# cm_plot_labels = ['> 7 days', '<= 7 days']
-# plot_confusion_matrix(cm, cm_plot_labels)
+def train(number_of_epochs=5):
+    # Don't pre-allocate GPU memory (Tensorflow 2)
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(gpus[0], True)
 
-# Build fine-tuned VGG-16 model
-vgg16_model = keras.applications.vgg16.VGG16()
+    output_path = 'Models/'
+    train_path = 'Q2/Train/'
+    valid_path = 'Q2/Valid/'
+    test_path = 'Q2/Test'
+    labels = ['G7', 'LE7']
 
-# Pop the last output layer
-vgg16_model.layers.pop()
+    # 112 training images
+    train_batches = ImageDataGenerator().flow_from_directory(train_path, target_size=(224, 224), classes=labels, batch_size=8)
 
-# Transform VGG-16 from type Model to Sequential
-model = Sequential()
-for layer in vgg16_model.layers:
-    model.add(layer)
+    # 16 validation images
+    valid_batches = ImageDataGenerator().flow_from_directory(valid_path, target_size=(224, 224), classes=labels, batch_size=8)
 
-# Freeze layers from future training so weights are not updated
-for layer in model.layers:
-    layer.trainable = False
+    # 14 test images
+    test_batches = ImageDataGenerator().flow_from_directory(test_path, target_size=(224, 224), classes=labels, batch_size=7)
 
-# Add an updated dense layer for the 2 categories
-model.add(Dense(2, activation='softmax'))
-# model.summary()
-del vgg16_model  # Clear memory
+    # Build fine-tuned VGG-16 model
+    vgg16_model = keras.applications.vgg16.VGG16()
 
-# Compile
-model.compile(Adam(lr=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+    # Pop the last output layer
+    vgg16_model.layers.pop()
 
-# Train
-epochs = 5
-start = time.time()
-model.fit_generator(train_batches, steps_per_epoch=14, validation_data=valid_batches, validation_steps=2, epochs=epochs, verbose=2)
-end = time.time()
-time_elapsed = end - start
-print("Time (s): %.3f" % time_elapsed)
+    # Transform VGG-16 from type Model to Sequential
+    model = Sequential()
+    for layer in vgg16_model.layers:
+        model.add(layer)
 
-# Save the model
-model_version = 0
-filename = "Model_%d.h5" % 0
-output_filepath = os.path.join(output_path, filename)
-while os.path.exists(output_filepath):
-    model_version += 1
+    # Freeze layers from future training so weights are not updated
+    for layer in model.layers:
+        layer.trainable = False
+
+    # Add an updated dense layer for the 2 categories
+    model.add(Dense(2, activation='softmax'))
+    # model.summary()
+    del vgg16_model  # Clear memory
+
+    # Compile
+    model.compile(Adam(lr=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # Train
+    epochs = 100
+    start = time.time()
+    model.fit_generator(train_batches, steps_per_epoch=14, validation_data=valid_batches, validation_steps=2, epochs=epochs, verbose=2)
+    end = time.time()
+    time_elapsed = end - start
+    print("Time (s): %.3f" % time_elapsed)
+
+    # Save the model
+    model_version = 0
+    filename = "Model_%d.h5" % 0
     output_filepath = os.path.join(output_path, filename)
+    while os.path.exists(output_filepath):
+        model_version += 1
+        output_filepath = os.path.join(output_path, filename)
 
-model.save(output_filepath)
+    model.save(output_filepath)
 
-print('Saved model %s' % output_filepath)
+    print('Saved model %s' % output_filepath)
+
+
+# Set number of epochs in the command line
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        epochs = sys.argv[1]
+        print(f"Number of epochs: {epochs}")
+        train(epochs)
+    else:
+        print("Missing number of epochs as input argument")
+
